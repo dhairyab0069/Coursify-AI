e74f08e8ee64957c9174c3c54c5af3cd5cc2cae8
Vinu Ihalagamage <vinu.i2017@gmail.com>
2024-02-01 01:33:42 -0800

    chosing Login or Resgister

--- tests/SettingsTest.py
+++ tests/SettingsTest.py
@@ -6,16 +6,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-LOGIN_PAGE_URL = "http://127.0.0.1:5000" 
+MAIN_PAGE_URL = "http://127.0.0.1:5000" 
+LOGIN_PAGE_URL = "http://127.0.0.1:5000/login"
 
 # The fixture 'browser' is set to have a 'function' scope, 
 # meaning every function requesting the 'browser'
 # a new instance of the Chrome browser will be created.
 @pytest.fixture(scope="function")
 def browser():
-    driver = webdriver.Chrome(executable_path='V:\Users\vinu\Desktop\SEM 2\COSC 499\chromedriver-win64\chromedriver.exe') #new instance of chrome
+    driver = webdriver.Chrome() #new instance of chrome
     yield driver 
     driver.quit()
+
+####################################################################
+def test_homepage(browser):
+    browser.get(MAIN_PAGE_URL)
+    chose_login = WebDriverWait(browser,10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "Login"))
+    )
+     
+    chose_login.click()
+     
+    try:
+        # Adjust the selector to match an element present only when logged in
+        email_present = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "email"))
+        )
+        # If the above line does not throw an exception, the login is considered successful
+        assert email_present, "Found the Login option!"
+    except TimeoutException:
+        assert False, "Did not find the Login in option"  # For real tests, this could raise an exception or fail the test
+
+
+####################################################################
+
+def test_login(browser):
+    my_email = "vinu.ubc@gmail.com"
+    my_password = "westbrook00"
+
+    login(browser, my_email, my_password)
+
+    try:
+        # Adjust the selector to match an element present only when logged in
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "head"))
+        )
+        # If the above line does not throw an exception, the login is considered successful
+        print("Login successful!")  # For real tests, use assert statements instead of print
+    except TimeoutException:
+        print("Login failed or timed out.")  # For real tests, this could raise an exception or fail the test
+
+######################################################################
+            
 
 #Reusable function to perform the login action
 def login(browser, email, password):
@@ -38,24 +80,10 @@ def login(browser, email, password):
         # is one of those predefined conditions
     )
 
-    # this or simply login_button.click();
-    browser.execute_script("arguments[0].click();", login_button)
+    login_button.click();
+    #browser.execute_script("arguments[0].click();", login_button)
 
-def test_login(browser):
-    my_email = "vinu.ubc@gmail.com"
-    my_password = "westbrook00"
 
-    login(browser, my_email, my_password)
-
-    try:
-        # Adjust the selector to match an element present only when logged in
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.ID, "head"))
-        )
-        # If the above line does not throw an exception, the login is considered successful
-        print("Login successful!")  # For real tests, use assert statements instead of print
-    except TimeoutException:
-            print("Login failed or timed out.")  # For real tests, this could raise an exception or fail the test
 
     
 
